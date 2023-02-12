b4487e532efe45861743a6d5c976991fbda3cb56
tokkaya79 <tokkaya79@gmail.com>
2023-02-12 12:37:48 +0200

    created books, book, types of book

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "src/app/app.component";
+import { BooksComponent } from 'src/app/books/books.component';
+import { BookComponent } from './book/book.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent, BooksComponent, BookComponent],
+  imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {}
---
+++ src/app/books/books.component.ts
@@ -0,0 +1,83 @@
+import { Component } from '@angular/core';
+import { Book } from 'src/app/types/book.interface';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.scss']
+})
+export class BooksComponent {
+
+  books: Book[] = [
+    {
+      name: 'The Seven Husbands of Evelyn Hugo',
+      author: 'Taylor Jenkins Reid',
+      img: 'https://www.britishbook.ua/upload/resize_cache/iblock/b5e/00tvje1em6ci4qlkxw3oue7hxc6p9rzg/295_448_174b5ed2089e1946312e2a80dcd26f146/kniga_the_seven_husbands_of_evelyn_hugo.jpg',
+      price: 700
+    },
+    {
+      name: 'It Ends With Us',
+      author: 'Colleen Hoover',
+      img: 'https://www.britishbook.ua/upload/resize_cache/iblock/ffd/06rzwvp4301pjxbur7smhms81fptvhvg/230_350_174b5ed2089e1946312e2a80dcd26f146/kniga_it_ends_with_us.jpg',
+      price: 800
+
+    },
+      {
+      name: 'The Seven Husbands of Evelyn Hugo',
+      author: 'Taylor Jenkins Reid',
+      img: 'https://www.britishbook.ua/upload/resize_cache/iblock/c8f/kxnjh2v1wpn3uokqx4qgnsnutsbjfafh/292_448_174b5ed2089e1946312e2a80dcd26f146/kniga_call_me_by_your_name_book_1_film_tie_in.jpg',
+      price: 700
+    },
+    {
+      name: 'It Ends With Us',
+      author: 'Colleen Hoover',
+      img: 'https://www.britishbook.ua/upload/resize_cache/iblock/05d/7afop4vkqbunrnofurzcg81j7mssrffu/428_640_174b5ed2089e1946312e2a80dcd26f146/kniga_harry_potter_and_the_prisoner_of_azkaban.jpeg',
+      price: 800
+
+    },
+      {
+      name: 'The Seven Husbands of Evelyn Hugo',
+      author: 'Taylor Jenkins Reid',
+      img: 'https://www.britishbook.ua/upload/resize_cache/iblock/ffe/j829f8bmdk76d4u7a9iw4a38ucyidncc/397_606_174b5ed2089e1946312e2a80dcd26f146/kniga_100_selected_stories_of_o_henry.jpg',
+      price: 700
+    },
+    {
+      name: 'It Ends With Us',
+      author: 'Colleen Hoover',
+      img: 'https://www.britishbook.ua/upload/resize_cache/iblock/93e/3kxybi37pg4ri8e7b3f0jq7bkt0mzdm6/283_430_174b5ed2089e1946312e2a80dcd26f146/kniga_looking_for_alaska.jpg',
+      price: 800
+
+    },
+      {
+      name: 'The Seven Husbands of Evelyn Hugo',
+      author: 'Taylor Jenkins Reid',
+      img: 'https://www.britishbook.ua/upload/resize_cache/iblock/c34/zytw3yozk8kdglhklc317mc793azb5ab/291_448_174b5ed2089e1946312e2a80dcd26f146/kniga_the_midnight_library.jpg',
+      price: 700
+    },
+    {
+      name: 'Violet Bent Backwards Over the Grass',
+      author: 'Lana Del Rey',
+      img: 'https://www.britishbook.ua/upload/resize_cache/iblock/2ef/jtrsao5tyop8tqibrvbm0ekr5r4larwh/292_448_174b5ed2089e1946312e2a80dcd26f146/kniga_violet_bent_backwards_over_the_grass.jpg',
+      price: 800
+
+    }
+  ]
+
+  searchName: string = ''
+
+  isDisabled: boolean = false
+  isShowing: boolean = true
+
+handleBtn(){
+ this.isDisabled = !this.isDisabled
+ }
+
+handleInput(event: any){
+  this.searchName = event.target.value
+
+}
+toggleBooks(){
+  this.isShowing = !this.isShowing
+}
+
+}
