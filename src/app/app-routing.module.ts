236b2833b404c8a184ce79f144720f6f6bdb2921
tokkaya79 <tokkaya79@gmail.com>
2023-02-14 11:12:11 +0200

    created login-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { BooksComponent } from "src/app/books/books.component";
 import { CartComponent } from "src/app/cart/cart.component";
+import { LoginComponent } from "src/app/auth/login/login.component";
+import { RegisterComponent } from "src/app/auth/register/register.component";
 
 const routes: Routes = [
   {path:'', component: BooksComponent},
-  {path:'cart', component: CartComponent}
+  {path:'cart', component: CartComponent},
+  {path:'login', component: LoginComponent},
+  {path:'register', component: RegisterComponent},
 ]
 
 @NgModule({
